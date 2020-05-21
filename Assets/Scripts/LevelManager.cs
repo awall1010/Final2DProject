8db9689ef0d257fcf7c0ba02a6870f5507fa3aa4
awall1010 <59846520+awall1010@users.noreply.github.com>
2020-05-20 20:35:51 -0700

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+// #CPSC236-Final Project
+// Kenneth Cho, Aidan Wall
+// 2325383,2317537
+// kecho@chapman.edu
+//awall@chapman.edu
+// cpsc236-03
+// This is the level manager that controls changing between levels
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
