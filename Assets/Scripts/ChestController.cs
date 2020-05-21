51d1b2354ea4176246df0b92a263a4a26b527196
Kenneth Cho <56805087+nykid99@users.noreply.github.com>
2020-05-20 19:58:59 -0700

    script update

--- Assets/Scripts/ChestController.cs
+++ Assets/Scripts/ChestController.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+  
+// #CPSC236-Final Project
+// Kenneth Cho, Aiden Wall
+// 2325383,2317537
+// kecho@chapman.edu
+// cpsc236-03
+// This is the CharacterController2D script
+// This is a ChestController script that controls chest interactables in the game
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,21 +18,21 @@ public class ChestController : MonoBehaviour
     private bool playerCollision;
     public bool alreadyPlayed = false;
     [SerializeField]
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer; //sprite renderer for chests
     [SerializeField]
-    private Sprite openSprite, closedSprite;
+    private Sprite openSprite, closedSprite; // sprites for when chest is closed and open
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 9)
         {
-            playerCollision = true;
+            playerCollision = true;//player has collided
         }
         if (playerCollision == true && alreadyPlayed == false)
         {
-            spriteRenderer.sprite = openSprite;
-            AudioSource.PlayClipAtPoint(ChestClip, transform.position);
+            spriteRenderer.sprite = openSprite; //sprite changes to the open sprite
+            AudioSource.PlayClipAtPoint(ChestClip, transform.position); //plays audio when collision occurs with the player
             playerCollision = false;
-            alreadyPlayed = true;
+            alreadyPlayed = true; //used to prevent audio and sprite change reactivating
 
         }
     }
